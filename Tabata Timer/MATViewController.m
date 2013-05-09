cb10960bb047fd3eece0f7e8450e9b35d85b7ea7
Gary Damm <garydamm@gmail.com>
2013-05-08 22:32:49 -0700

    implementing simple work and rest

--- Tabata Timer/MATViewController.m
+++ Tabata Timer/MATViewController.m
@@ -1,41 +1,101 @@
 
 #import "MATViewController.h"
-#import "MATWork.h"
 
 @interface MATViewController ()
 
-@property (nonatomic) MATWork *work;
+@property (nonatomic) NSTimer *workTimer;
+@property (nonatomic) NSTimer *restTimer;
+@property (nonatomic) int remainingWorkTime;
+@property (nonatomic) int remainingRestTime;
+@property (nonatomic) BOOL working;
+@property (nonatomic) BOOL resting;
 
 @end
 
 @implementation MATViewController
 
+int workTime = 4;
+int restTime = 2;
+
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    
-    if (self.work) {
-        [self.work reset];
-    }
-    else {
-        self.work = [[MATWork alloc] initWithViewController:self];
-    }
+    [self reset];
 }
 
 - (void)viewDidDisappear:(BOOL)animated {
-    if (self.work) {
-        [self.work reset];
-    }
+    [self reset];
 }
 
 - (IBAction)startStopButtonClicked:(id)sender {
-    if (self.work) {
-        [self.work start];
-    }
+    [self start];
 }
 
 - (IBAction)resetButtonClicked:(id)sender {
-    if (self.work) {
-        [self.work reset];
+    [self reset];
+}
+
+- (void)rest {
+    self.resting = YES;
+    self.working = NO;
+    self.workRestLabel.text = @"Resting";
+    self.timeLabel.text = [NSString stringWithFormat:@"%d",self.remainingRestTime];
+    [self.workTimer invalidate];
+    self.restTimer = [NSTimer scheduledTimerWithTimeInterval:1.0
+                                                      target:self
+                                                    selector:@selector(restTimerTicked:)
+                                                    userInfo:nil
+                                                     repeats:YES];
+}
+
+- (void)start {
+    if (!self.working && !self.resting){
+        self.working = YES;
+        self.workRestLabel.text = @"Working";
+        [self.startStopButton setTitle:@"Running" forState:UIControlStateDisabled];
+        [self.restTimer invalidate];
+        self.workTimer = [NSTimer scheduledTimerWithTimeInterval:1.0
+                                                          target:self
+                                                        selector:@selector(workTimerTicked:)
+                                                        userInfo:nil
+                                                         repeats:YES];
+    }   
+}
+
+- (void)stop {
+    [self.workTimer invalidate];
+    [self.restTimer invalidate];
+    [self.startStopButton setTitle:@"Start" forState:UIControlStateNormal];
+}
+
+- (void)reset {
+    [self stop];
+    self.working = NO;
+    self.resting = NO;
+    self.workRestLabel.text = @"Stopped";
+    self.remainingWorkTime = workTime;
+    self.remainingRestTime = restTime;
+    self.timeLabel.text = [NSString stringWithFormat:@"%d",self.remainingWorkTime];
+    
+}
+
+- (void)workTimerTicked:(NSTimer*)timer {
+    if (self.remainingWorkTime > 0) {
+        self.remainingWorkTime -= 1;
+        self.timeLabel.text = [NSString stringWithFormat:@"%d",self.remainingWorkTime];
+    }
+    else {
+        
+        [self rest];
+    }
+}
+
+- (void)restTimerTicked:(NSTimer*)timer {
+    if (self.remainingRestTime > 0) {
+        self.remainingRestTime -= 1;
+        self.timeLabel.text = [NSString stringWithFormat:@"%d",self.remainingRestTime];
+    }
+    else {
+        [self reset];
     }
 }
 
